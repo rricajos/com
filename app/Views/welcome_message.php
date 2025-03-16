7f188b113f02632ab354a8576040b99881fe3162
Ricard Penin Honrubia <202694591+SuperYayas@users.noreply.github.com>
2025-03-16 18:12:43 +0100

    Update welcome_message.php

--- app/Views/welcome_message.php
+++ app/Views/welcome_message.php
@@ -225,7 +225,7 @@
 
     <div class="heroe">
 
-        <h1>Welcome to CodeIgniter <?= CodeIgniter\CodeIgniter::CI_VERSION ?></h1>
+        <h1>Welcome to IFP Grupo 8 <?= CodeIgniter\CodeIgniter::CI_VERSION ?></h1>
 
         <h2>The small framework with powerful features</h2>
 
